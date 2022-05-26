0c60ca43440c93f61787f4ceb7c9244418faea4f
Pavel Egorov <egorovpvspb@gmail.com>
2022-05-26 16:39:33 +0300

    all 9 tests

--- sodaMachine.js
+++ sodaMachine.js
@@ -5,12 +5,21 @@ class SodaMachine {
   }
 
   currentInventoryCount() {
+    return this.sodas.length; //возвращаем кол-во напитков в массиве sodas: [pepsi, mountainDew, cokeZero, secondPepsi]
   }
 
   findSoda(sodaBrand) {
+    return this.sodas.find(el => el.brand === sodaBrand);// с помощью метода find() возвращаем значение первого найденного в массиве элемента, значание которое запросил пользователь
   }
 
   sell(sodaBrand) {
+  //нужно найти индекс газировки чтобы по нему удалить ее из автомата
+    this.sodas.find((el, i) => {
+        if(el.brand === sodaBrand) {
+         this.sodas.splice(i, 1);
+         return this.cash += el.price;
+        } 
+      })
   }
 }
 
